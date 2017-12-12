ed2db021295c67415602f0cfac8d327e1051f548
amandarenee1 <arw0219@gmail.com>
2017-12-12 00:03:30 +0000

    automatically backed up by learn

--- hello_world.rb
+++ hello_world.rb
@@ -1,8 +1,2 @@
 puts "Hello World!"
-ruby hello_world.#!/usr/bin/env ruby -wKU
-puts "Hello World!"
-ruby hello_world.#!/usr/bin/env ruby -wKU
-Learn
-puts "Hello World!"
-ruby hello_world.#!/usr/bin/env ruby -wKU
-puts "Hello World!"
+
